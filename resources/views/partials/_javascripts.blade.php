e04a3aafd94ca8d179966104a5927d49d5d77bff
gamwebdev <ubajpai1aa@gmail.com>
2017-02-11 03:20:36 -0800

    Revert " new fancy Blog"
    This reverts commit 31cb66e958ff472a28dc3ef850bde8bc35b18aae.

--- resources/views/partials/_javascripts.blade.php
+++
@@ -1,3 +0,0 @@
-    <script src="{{ asset('js/jquery-3.0.0.min.js') }}"></script>
-  <script src="{{ asset('js/tether.min.js') }}"></script>
-    <script src="{{ asset('js/bootstrap.min.js') }}"></script>
